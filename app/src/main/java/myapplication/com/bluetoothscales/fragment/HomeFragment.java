8b82591c375b0872df936b18043afd92207cd9c5
yangsong <yang3384046@126.com>
2017-09-08 17:36:21 +0800

    9.08

--- app/src/main/java/myapplication/com/bluetoothscales/fragment/HomeFragment.java
+++ app/src/main/java/myapplication/com/bluetoothscales/fragment/HomeFragment.java
@@ -1,13 +1,32 @@
 package myapplication.com.bluetoothscales.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import myapplication.com.bluetoothscales.R;
+import myapplication.com.bluetoothscales.activity.PregActivity;
 import myapplication.com.bluetoothscales.base.BaseFragment;
 
 
 public class HomeFragment extends BaseFragment {
+
+    @BindView(R.id.home_weight)
+    TextView homeWeight;
+    @BindView(R.id.home_lbs)
+    TextView homeLbs;
+    @BindView(R.id.home_bmi)
+    TextView homeBmi;
+    @BindView(R.id.home_rou)
+    TextView homeRou;
+    @BindView(R.id.home_jirou)
+    TextView homeJirou;
+    @BindView(R.id.home_guge)
+    TextView homeGuge;
+
 
     @Override
     protected void initData(View layout, Bundle savedInstanceState) {
@@ -17,5 +36,11 @@ public class HomeFragment extends BaseFragment {
     @Override
     protected int getContentView() {
         return R.layout.fragment_home;
+    }
+
+
+    @OnClick(R.id.home_mode)
+    public void onViewClicked() {
+        startActivity(new Intent(getActivity(), PregActivity.class));
     }
 }
